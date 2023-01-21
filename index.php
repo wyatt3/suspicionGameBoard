cb17dd587da0d1baee087fc2e0f870fa8e0e3961
Wyatt Johnson <wyatt.j1834@gmail.com>
2023-01-21 11:16:11 -0700

    Server Reset Commit

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 <body>
   <div class="check-button-container">
     <?php for($x = 0;$x < 10;$x++) {?>
-      <button class="check-button" id="check-<?php echo $x; ?>" onClick="checkAll(<?php echo $x; ?>)">Check All</button>
-      <button class="check-button" id="uncheck-<?php echo $x; ?>" onClick="uncheckAll(<?php echo $x; ?>)" style="display: none;">Un-Check All</button>
+      <button class="check-button" id="check-<?php echo $x; ?>" onClick="checkAll(<?php echo $x; ?>)">&#x2718; All</button>
+      <button class="check-button" id="uncheck-<?php echo $x; ?>" onClick="uncheckAll(<?php echo $x; ?>)" style="display: none;">&#x274f; All</button>
     <?php } ?>
   </div>
   <?php for($i = 0;$i < 10;$i++) {?>
@@ -21,33 +21,54 @@
       <img src="suspicion.png" alt="">
       <div class="characters">
         <?php for($j = 0;$j < 10;$j++) {?>
-          <div class="character <?php echo 'character-' . $j; ?>">
-            <input class="character-checkbox-<?php echo $j; ?>" type="checkbox">
+          <div class="character character-<?php echo $j; ?>" data-state="unchecked">
           </div>
           <?php } ?>
       </div>
     </div>
   <?php } ?>
-  <button onClick="lockInAnswers();">Lock In</button>
+  <button class="lock-in" onClick="if(window.confirm('Are you sure you want to lock in your answers?')) {lockInAnswers();}">Lock In</button>
 
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
+    var locked = false;
     function lockInAnswers() {
       $('input').prop('disabled', true);
       $('button').prop('disabled', true);
+      locked = true;
     }
 
     function checkAll(character) {
-      $('.character-checkbox-' + character).prop('checked', true);
+      $('.character-' + character).attr('data-state', 'checked');
       $('#check-' + character).hide();
       $('#uncheck-' + character).show();
     }
 
     function uncheckAll(character) {
-      $('.character-checkbox-' + character).prop('checked', false);
+      $('.character-' + character).attr('data-state', 'unchecked');
       $('#check-' + character).show();
       $('#uncheck-' + character).hide();
     }
+
+    $('.character').click(function() {;
+    if(!locked) {
+      if($(this).attr('data-state') == 'unchecked') {
+        $(this).attr('data-state', 'guess');
+      }
+      else if($(this).attr('data-state') == 'guess') {
+        $(this).attr('data-state', 'checked');
+      }
+      else if($(this).attr('data-state') == 'checked') {
+        $(this).attr('data-state', 'unchecked');
+      }
+    }
+    });
+
+    var int = setInterval(function() {
+      $('.character[data-state="unchecked"]').css('opacity', 0);
+      $('.character[data-state="guess"]').css('opacity', .5);
+      $('.character[data-state="checked"]').css('opacity', .9);
+    }, 10);
   </script>
 </body>
 </html>
